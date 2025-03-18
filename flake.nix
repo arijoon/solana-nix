31eecc87c99c6a5cc67ad16138f458cd807d8d94
0xPlish <plish@hylo.so>
2025-03-18 14:37:33 -0700

    Use oxalica overlay; add packages from local

--- flake.nix
+++ flake.nix
@@ -1,23 +1,41 @@
 {
   description = "Solana development setup with Nix.";
   inputs = {
-    nixpkgs = ./pkgs.nix;
-    flake-parts.url = "github:hercules-ci/flake-parts/f4330d22f1c5d2ba72d3d22df5597d123fdb60a9";
+    nixpkgs.url =
+      "github:nixos/nixpkgs/c5e2528c7c4ec05ce05a563e3be64f3525b278ad";
+    flake-parts.url =
+      "github:hercules-ci/flake-parts/f4330d22f1c5d2ba72d3d22df5597d123fdb60a9";
+    rust-overlay.url =
+      "github:oxalica/rust-overlay/87f0965f9f5b13fca9f38074eee8369dc767550d";
   };
-  outputs = inputs@{ self, nixpkgs, flake-parts }:
+  outputs = inputs@{ self, nixpkgs, flake-parts, rust-overlay }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin"
-      "aarch64-darwin" "x86_64-windows" ];
-      perSystem = { config, self', inputs', pkgs, system }: {
-        devShells.default = with pkgs; mkShell {
-          packages = [
-            anchor-cli
-            solana-cli
-            solana-rust
-            yarn
-            nodejs
-          ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+        "x86_64-windows"
+      ];
+      perSystem = { config, self', inputs', nixpkgs', rust-overlay', system }:
+        with import nixpkgs' {
+          overlays = [ rust-overlay'.overlays.default ./pkgs ];
         };
-      };
+        let
+          solana-source = self.callPackage (import ./solana-source.nix) { };
+          solana-platform-tools =
+            self.callPackage (import ./solana-platform-tools.nix) {
+              inherit solana-source;
+            };
+          solana-rust = self.callPackage (import ./solana-rust.nix) {
+            inherit solana-platform-tools;
+          };
+          solana-cli = self.callPackage (import ./solana-cli.nix) { };
+          anchor-cli = self.callPackage (import ./anchor-cli.nix) { };
+        in {
+          devShells.default = mkShell {
+            packages = [ anchor-cli solana-cli solana-rust yarn nodejs ];
+          };
+        };
     };
 }
