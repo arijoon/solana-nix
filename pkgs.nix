1a9aa46e77b65c8590da187635207cfa89bf153b
Francesco Dainese <franci.dainese@gmail.com>
2025-04-28 20:18:16 +0200

    refactor: use flake-compat for pkgs.nix
    chore: cleanup niv sources

--- pkgs.nix
+++ pkgs.nix
@@ -1,21 +1,26 @@
 {
-  sources ? import ./nix/sources.nix,
   config ? { },
   system ? builtins.currentSystem,
 }:
 let
-  nixpkgs = sources.nixpkgs;
-  rust_overlay = import (sources.rust_overlay);
-
-  overlays = [
-    (self: super: {
-      solana-source = self.callPackage (import ./solana-source.nix) { };
-      solana-platform-tools = self.callPackage (import ./solana-platform-tools.nix) { };
-      solana-rust = self.callPackage (import ./solana-rust.nix) { };
-      solana-cli = self.callPackage (import ./solana-cli.nix) { };
-      anchor-cli = self.callPackage (import ./anchor-cli.nix) { };
-    })
-    rust_overlay
-  ];
+  flake =
+    (import (
+      let
+        lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+        nodeName = lock.nodes.root.inputs.flake-compat;
+      in
+      fetchTarball {
+        url =
+          lock.nodes.${nodeName}.locked.url
+            or "https://github.com/edolstra/flake-compat/archive/${lock.nodes.${nodeName}.locked.rev}.tar.gz";
+        sha256 = lock.nodes.${nodeName}.locked.narHash;
+      }
+    ) { src = ./.; }).outputs;
 in
-import nixpkgs { inherit config system overlays; }
+import flake.inputs.nixpkgs {
+  inherit config system;
+  overlays = [
+    flake.overlays.default
+    flake.inputs.rust-overlay.overlays.default
+  ];
+}
